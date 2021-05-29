d4b4c21738b85f451c73e5ff006413d7634b4975
Merick Nugroho <mericknugroho@gmail.com>
2021-05-29 19:58:54 +0700

    tambah func check_form

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -32,7 +32,7 @@ class Test extends CI_Controller
 		$id = input('id_modul');
 		$batas_waktu = input('waktu_tes');
 		// jumlah soal yg ditampilkan
-		$n_soal = 3;
+		$n_soal = 4;
 
 		$dateTime = new DateTime();
 		$dateTime->modify("+{$batas_waktu} minutes");
@@ -65,17 +65,169 @@ class Test extends CI_Controller
 				array_push($soal_acak, $soal_dumy);
 			}
 			$data['soal_acak'] = $soal_acak;
-			// echo "bank soal = ";
-			// echo "<br>";
-			// var_dump($bank_soal);
-			// echo "soal sudah acak = ";
-			// echo "<br>";
-			// var_dump($soal_acak);
-			// echo "sistem acak array = ";
+
+			/**
+			 * ------------------------------------------
+			 * Format Penyusunan Jawaban
+			 * ------------------------------------------
+			 * $pg = array();
+			 * $key = array(
+			 *	 'Sekolah Menengah Atas',
+			 *	 'Sekolah Menengah Bawah',
+			 *	 'Sekolah Menengah Kiri',
+			 *	 'Sekolah Menengah Kanan'
+			 *	);
+			 *	$n = range('a', 'd');
+			 *	for ($i = 0; $i < count($key); $i++) {
+			 *		$pg[$i]['kunci_jawaban'] = $n[$i];
+			 *		$pg[$i]['jawaban'] = $key[$i];
+			 * 
+			 * var_dump(serialize($pg));
+			 * die;
+			 *	}
+			 */
+			/**
+			 * ------------------------------------------
+			 * Format Penyusunan Array untuk mix soal
+			 * ------------------------------------------
+			 *$arr = array(
+			 *2, 3, 4
+			 *);
+			 * 
+			 * var_dump(serialize($pg));
+			 * die;
+			 *	}
+			 */
+
+			/* ------------------------------------------
+			 * Format Penyusunan Lampiran
+			 * ------------------------------------------
+			 * ---------------gambar---------------------
+			 * $arr = array(
+			 *'tipe' => 'gambar',
+			 *'link' => 'https://drive.google.com/file/d/1-uOP0BEMy5exY44tNoeIEEcN2rv0gO2Y/view?usp=sharing'
+			 * );
+			 *---------------audio---------------------
+			 * $arr = array(
+			 *'tipe' => 'audio',
+			 *'link' => 'https://drive.google.com/file/d/1-uOP0BEMy5exY44tNoeIEEcN2rv0gO2Y/view?usp=sharing'
+			 * );
+			 * var_dump(serialize($pg));
+			 * die;
+			 */
+
+			/* ------------------------------------------
+			 * Format Penyusunan soal multiple
+			 * ------------------------------------------
+			* $data['soal_multiple'] = array(
+			* array(
+			* 'row' => 'apple',
+			* 'column' => 'fruit'
+			* ),
+			* array(
+			* 'row' => 'truck',
+			* 'column' => 'vehicle'
+			* ),
+			* array(
+			* 'row' => 'computer',
+			* 'column' => 'tech'
+			* ),
+			* array(
+			* 'row' => 'teacher',
+			* 'column' => 'helper'
+			* ),
+			* array(
+			* 'row' => 'cake',
+			* 'column' => 'desert'
+			* )
+			* );
+			 */
+
+			// $arr = array(
+			// 	'tipe' => 'video',
+			// 	'link' => 'https://drive.google.com/file/d/1HJmcAcmVdiDp5WVx-XNroB5Fy82oTBrS/view?usp=sharing'
+			// );
+
+			// var_dump(serialize($arr));
+			// die;
+
+			// foreach ($data['soal_multiple'] as $value) {
+			// 	$rowfix[] = $value['column'];
+			// }
+			// shuffle($rowfix);
+			// $data['rowfix'] = $rowfix;
+			// var_dump($rowfix);
+			// var_dump(serialize($data['soal_multiple']));
+			// die;
 
 			$this->session->set_userdata('soal', $soal_acak);
 			$this->load->view($page, $data);
 		}
+	}
+
+	function check_form()
+	{
+		//statmn data
+		$result = 0;
+		$nilai = 0;
+		$soal = $this->session->userdata('soal');
+		$jawaban_siswa = $this->input->post();
+		$jawaban = array_values($jawaban_siswa['answer']);
+		//end of statmn data
+
+		// penghitungan nilai
+		foreach ($jawaban  as $key1 => $value1) {
+			if ($value1['tipe'] == 1) {
+				//soal tipe jawaban Pilgan
+				if (!empty($value1['jwb'])) {
+					if (($key2 = array_search(1, array_column($soal, 'soal_tipe'))) !== false) {
+						if ($value1['jwb'] === $soal[$key2]['soal_kunci']) {
+							$nilai++;
+						}
+					}
+				}
+			} elseif ($value1['tipe'] == 2) {
+				//soal tipe true-false
+				if (!empty($value1['jwb'])) {
+					if (($key2 = array_search(2, array_column($soal, 'soal_tipe'))) !== false) {
+						if ($value1['jwb'] === $soal[$key2]['soal_kunci']) {
+							$nilai++;
+						}
+					}
+				}
+			} elseif ($value1['tipe'] == 4) {
+				//soal tipe jawaban singkat
+				if (!empty($value1['jwb'])) {
+					if (($key2 = array_search(4, array_column($soal, 'soal_tipe'))) !== false) {
+						if ($value1['jwb'] === $soal[$key2]['soal_kunci']) {
+							$nilai++;
+						}
+					}
+				}
+			}
+		}
+
+		//penghitungan nilai tipe 5
+		if (($key3 = array_search(5, array_column($jawaban, 'tipe'))) !== false) {
+			if (($key4 = array_search(5, array_column($soal, 'soal_tipe'))) !== false) {
+				$kunci = unserialize($soal[$key4]['soal_kunci']);
+				foreach ($kunci as $value) {
+					if ($jawaban[$key3][$value['row']] == $value['column']) {
+						$result++;
+					}
+				}
+				if ($result == 5) {
+					$nilai++;
+				}
+			}
+		}
+		// end of tipe 5
+
+		$nilai_fix = ($nilai / count($soal)) * 100;
+		// end of penghitungan nilai
+		var_dump($nilai);
+		var_dump($nilai_fix);
+		die;
 	}
 
 
@@ -88,7 +240,9 @@ class Test extends CI_Controller
 		$data['soal_detail'] = $soal[$no - 1]['soal_detail'];
 		$data['soal_tipe'] = $soal[$no - 1]['soal_tipe'];
 		if ($soal[$no - 1]['soal_tipe'] == 1) {
-			$data['soal_jawaban'] = unserialize($soal[$no - 1]['soal_pg']);
+			$kunci_jawaban	= unserialize($soal[$no - 1]['soal_pg']);
+			shuffle($kunci_jawaban);
+			$data['soal_jawaban'] = $kunci_jawaban;
 		}
 
 		echo json_encode($data);
