4f6c36b77ae2b2c9a02401bb770858629b5eb0a4
Imam Maulana Ibrahim <imamaulana26.github@gmail.com>
2021-06-12 18:45:39 +0700

    add menu data module

--- application/views/admin/layouts/v_sidebar.php
+++ application/views/admin/layouts/v_sidebar.php
@@ -9,14 +9,14 @@
 	<!-- Sidebar -->
 	<div class="sidebar">
 		<!-- Sidebar user panel (optional) -->
-		<div class="user-panel mt-3 pb-3 mb-3 d-flex">
+		<!-- <div class="user-panel mt-3 pb-3 mb-3 d-flex">
 			<div class="image">
 				<img src="<?= base_url('assets/dist/') . 'img/user2-160x160.jpg' ?>" class="img-circle elevation-2" alt="User Image">
 			</div>
 			<div class="info">
 				<a href="#" class="d-block">Alexander Pierce</a>
 			</div>
-		</div>
+		</div> -->
 
 		<!-- Sidebar Menu -->
 		<nav class="mt-2">
@@ -28,6 +28,24 @@
 						<i class="fa fa-fw fa-desktop nav-icon"></i>
 						<p>Dashboard</p>
 					</a>
+				</li>
+
+				<li class="nav-item has-treeview">
+					<a href="#" class="nav-link">
+						<i class="nav-icon fas fa-folder"></i>
+						<p>
+							Data Module
+							<i class="fas fa-angle-left right"></i>
+						</p>
+					</a>
+					<ul class="nav nav-treeview">
+						<li class="nav-item">
+							<a href="<?= site_url('test') ?>" class="nav-link" id="test">
+								<i class="far fa-circle nav-icon"></i>
+								<p>Kelola Soal</p>
+							</a>
+						</li>
+					</ul>
 				</li>
 
 				<li class="nav-item has-treeview">
