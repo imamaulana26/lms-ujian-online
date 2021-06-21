712f446fbe02b7bad84b73bfaf94902c0fa8ab51
Imam Maulana Ibrahim <imamaulana26.github@gmail.com>
2021-06-21 13:55:22 +0700

    penambahan module kelola soal

--- application/config/routes.php
+++ application/config/routes.php
@@ -54,3 +54,4 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
 $route['kelola-module'] = 'kelola_module';
+$route['kelola-module/soal/(:num)'] = 'kelola_module/soal/$1';
--- application/views/admin/v_kelola_module.php
+++ application/views/admin/v_kelola_module.php
@@ -67,9 +67,9 @@
 														<span class="text-success mx-1" onclick="edit('<?= $li['id_modul'] ?>')" data-toggle="tooltip" data-placement="top" title="Ubah Module" style="cursor: pointer;">
 															<i class="fa fa-md fa-edit"></i>
 														</span>
-														<span class="text-primary mx-1" onclick="kelola_soal('<?= $li['id_modul'] ?>')" data-toggle="tooltip" data-placement="top" title="Kelola Soal" style="cursor: pointer;">
+														<a class="text-primary mx-1" href="<?= site_url('kelola-module/soal/' . $li['id_modul']) ?>" data-toggle="tooltip" data-placement="top" title="Kelola Soal" style="cursor: pointer;">
 															<i class="fa fa-md fa-question-circle"></i>
-														</span>
+														</a>
 														<span class="text-danger mx-1" onclick="hapus('<?= $li['id_modul'] ?>')" data-toggle="tooltip" data-placement="top" title="Hapus Module" style="cursor: pointer;">
 															<i class="fa fa-md fa-trash"></i>
 														</span>
@@ -213,8 +213,8 @@
 
 		$.ajax({
 			url: url,
-			data: $(this).serialize(),
-			type: $(this).attr("method"),
+			data: $('#form-module').serialize(),
+			type: "POST",
 			dataType: 'JSON',
 			success: function(hasil) {
 				console.log(hasil);
