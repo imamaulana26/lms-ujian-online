a308a96bb5531341087f3e7e1fdb58565388f38b
Imam Maulana Ibrahim <imamaulana26.github@gmail.com>
2021-07-03 19:01:31 +0700

    create controller auth

---
+++ application/controllers/Auth.php
@@ -0,0 +1,47 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+	public function index()
+	{
+		$page = 'v_login';
+		$data['title'] = 'Login';
+
+		$this->load->view($page, $data);
+	}
+
+	public function login()
+	{
+		$user = input('username');
+		$pass = input('password');
+
+		$get_user = $this->db->get_where('tbl_pengguna', ['pengguna_username' => $user, 'pengguna_password' => md5($pass)]);
+
+		if ($get_user->num_rows() > 0) {
+
+			$dt_user = $get_user->row_array();
+
+			if ($dt_user['pengguna_level'] == 2) {
+				$sess['nm_user'] = $dt_user['pengguna_nama'];
+				$sess['username'] = $dt_user['pengguna_username'];
+				$this->session->set_userdata($sess);
+				redirect('test');
+			} elseif ($dt_user['pengguna_level'] == 3) {
+				$sess['nm_user'] = $dt_user['pengguna_nama'];
+				$sess['username'] = $dt_user['pengguna_username'];
+
+				$this->session->set_userdata($sess);
+
+				redirect('dashboard');
+			}
+		} else {
+			echo 'password salah';
+		}
+	}
+
+	public function logout()
+	{
+		
+	}
+}
