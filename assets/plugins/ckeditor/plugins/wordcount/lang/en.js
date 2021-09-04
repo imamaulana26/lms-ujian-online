9e0080685e0860a081b8622b71231c53312f0211
Merick Nugroho <mericknugroho@gmail.com>
2021-09-05 00:14:45 +0700

    add plugnis ckeditor

---
+++ assets/plugins/ckeditor/plugins/wordcount/lang/en.js
@@ -0,0 +1,17 @@
+﻿/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang('wordcount', 'en', {
+    WordCount: 'Words:',
+    WordCountRemaining: 'Words remaining',
+    CharCount: 'Characters:',
+    CharCountRemaining: 'Characters remaining',
+    CharCountWithHTML: 'Character:',
+    CharCountWithHTMLRemaining: 'Characters (with HTML) remaining',
+    Paragraphs: 'Paragraphs:',
+    ParagraphsRemaining: 'Paragraphs remaining',
+    pasteWarning: 'Melebihi Batas Maksimal',
+    Selected: 'Selected: ',
+    title: 'Statistics'
+});
